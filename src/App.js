e6f47f8e9f99c42c08e79729e135fa812a98030f
Ariel <ariel.wm1@gmail.com>
2020-07-11 16:45:39 -0400

    Query function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Header from './components/Header'
 import Form from './components/Form'
 
 function App() {
+  const [search, saveSearch] = useState({
+    city: '',
+    country: '',
+  })
+
+  const [query, saveQuery] = useState(false)
+
+  const { city, country } = search
+
+  useEffect(() => {
+    console.log(city)
+  }, [query])
+
   return (
     <Fragment>
       <Header title="Weather APP" />
@@ -10,7 +23,11 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Form />
+              <Form
+                search={search}
+                saveSearch={saveSearch}
+                saveQuery={saveQuery}
+              />
             </div>
             <div className="col m6 s12">2</div>
           </div>
